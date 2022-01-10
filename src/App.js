0766d61cae364eb9a4c2eaff47d2abb7cd98560d
AAlkandari <96235631+AAlkandari@users.noreply.github.com>
2022-01-10 23:46:44 +0300

    Done

--- src/App.js
+++ src/App.js
@@ -4,15 +4,20 @@ import ProductList from "./components/ProductList";
 //styling
 import "./App.css"
 import Detail from './components/Detail'
+import { Routes , Route } from "react-router-dom"
+import NavBar from "./components/NavBar";
+
 
 
 function App() {
-
   return (
-    <div >
-      <Home />
-      <ProductList />
-      <Detail />
+    <div>
+    <NavBar/>
+     <Routes>
+     <Route path="/" element ={<Home />}></Route>
+      <Route path="/product-list" element ={<ProductList />}></Route>
+      <Route path="/productlist/:slug" element={<Detail />} />
+      </Routes>
     </div>
   );
 }
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,19 +1,26 @@
 import React from 'react'
+import { useParams } from "react-router-dom";
+import products from "../products";
+import { useEffect } from "react";
 
 function Detail() {
 
-    let cookie
+    const { slug } = useParams();
+
+    let cookie = products.find((p) => p.slug === slug);
+    useEffect(() => {
+      document.title = `${cookie.name}`;
+    });
     return (
-        <div className="detail">
-            {cookie && // if there is cookie show its data 
-                <>
-                    <h5>{cookie.name}</h5>
-                    <img src={cookie.image} alt="cookie" />
-                    <p>{cookie.price} KD</p>
-                    <p>{cookie.description}</p>
-                </>}
-
-
+      <div className="detail">
+        {cookie && ( // if there is cookie show its data
+          <>
+            <h5>{cookie.name}</h5>
+            <img src={cookie.image} alt="cookie" />
+            <p>{cookie.price} KD</p>
+            <p>{cookie.description}</p>
+          </>
+        )}
         </div>
     )
 }
